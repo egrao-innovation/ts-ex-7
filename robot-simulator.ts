a7421d18dd7eba6465a2877f5d461c4f8050a154
egrao-innovation <em.grao@innovation.es>
2022-03-31 11:27:17 +0200

    done

--- robot-simulator.ts
+++ robot-simulator.ts
@@ -4,24 +4,54 @@ export class InvalidInputError extends Error {
     this.message = message || 'Invalid Input'
   }
 }
-
+// string literal type guard
 type Direction = 'north' | 'east' | 'south' | 'west'
 type Coordinates = [number, number]
 
 export class Robot {
+  robotDirection:Direction = 'north';
+  robotCoordinates:Coordinates = [0, 0];
+  array = ['north','east','south','west'];
+
   get bearing(): Direction {
-    throw new Error('Remove this statement and implement this function')
+    return this.robotDirection;
   }
 
   get coordinates(): Coordinates {
-    throw new Error('Remove this statement and implement this function')
+    return this.robotCoordinates;
   }
 
-  place({}: { x: number; y: number; direction: string }) {
-    throw new Error('Remove this statement and implement this function')
+  place(position: { x: number; y: number; direction: string }) {
+      if(!this.array.includes(position.direction, 0)){
+        throw new InvalidInputError(position.direction);
+      }
+      this.robotDirection = position.direction as Direction;
+      this.robotCoordinates = [position.x, position.y];
   }
 
   evaluate(instructions: string) {
-    throw new Error('Remove this statement and implement this function')
+    for (var i = 0; i < instructions.length; i++) {
+      if(instructions.charAt(i) === 'R'){
+        this.robotDirection = ((this.array.indexOf(this.robotDirection)+1) < this.array.length) ? this.array[this.array.indexOf(this.robotDirection)+1] as Direction : this.array[0] as Direction;
+        ;
+      }
+      else if(instructions.charAt(i) === 'L'){
+        this.robotDirection = ((this.array.indexOf(this.robotDirection)-1) >= 0) ? this.array[this.array.indexOf(this.robotDirection)-1] as Direction : this.array[this.array.length-1] as Direction;
+      }
+      else if(instructions.charAt(i) === 'A'){
+        if(this.robotDirection === 'north'){
+          this.robotCoordinates[1] += 1;
+        }
+        else if(this.robotDirection === 'south'){
+          this.robotCoordinates[1] -= 1;
+        }
+        else if(this.robotDirection === 'east'){
+          this.robotCoordinates[0] += 1;
+        }
+        else if(this.robotDirection === 'west'){
+          this.robotCoordinates[0] -= 1;
+        }
+      }
+    }
   }
 }
